11cf65122401ae125b49e2dcb6df53c24d66ce43
mikok <84585119+miko1034@users.noreply.github.com>
2025-04-28 18:13:35 +0100

    maybe main function works idk i got rate limited

--- tg_token_scraper/main.py
+++ tg_token_scraper/main.py
@@ -6,8 +6,8 @@ import re
 #  - UNAUTHENTICATED = 60 per hour per IP addr
 #  - AUTHENTICATED = 1500 per hour
 
-def get_recently_updated(query):
-    tg_tokens = []
+def main(query):
+    tg_tokens = {}
 
     r = requests.get(f"https://github.com/search?q={query}&type=repositories&s=updated&o=desc")
     soup = BeautifulSoup(r.content, "html.parser")
@@ -20,18 +20,43 @@ def get_recently_updated(query):
     for el in repo_elements:
         if "/" in el.text:
             repo_names.append(el.text)
+    print(f"Found {len(repo_names)} repos.")
     
     repo_links = []
     for i in repo_names:
         owner,repo = i.split("/")
         json = requests.get(f"https://api.github.com/repos/{owner}/{repo}").json()
+        
+        try:
+            if "API rate limit" in json["message"]:
+                print("Rate limit reached. Exiting...")
+                quit()
+        except KeyError:
+            pass
+
         # get first 3 and last 3 commits
         commits_url = json["commits_url"]
-
         all_commits = requests.get(commits_url.replace("{/sha}", "")).json()
-        #if all_commits["mesasge"] == "Git Repository is empty.":
-        #    print("repo empty")
-        
+        print(f"Retrieved all commits for {owner}/{repo}")
+
+        # if github repo is empty
+        try:
+            if "Git Repository is empty." in all_commits["message"]:
+                print(f"{owner}/{repo} is empty. Skipping..")
+                continue
+        except TypeError:
+            pass
+
+
+        # if api rate limit exceeded
+        try:
+            if "API rate limit exceeded for" in all_commits["message"]:
+                pass
+        except KeyError and TypeError:
+            print("Api limit reached. Please change IP.")
+            break
+
+
         # first 3, last 3
         commits_to_search = all_commits[:3] + all_commits[len(all_commits)-3:]
 
@@ -41,11 +66,16 @@ def get_recently_updated(query):
         for commit in commits_to_search:
             html_url = commit["html_url"]
             commit_content = requests.get(html_url).text
-            tg_tokens.append(re.findall(r"\d{9}:[a-zA-Z0-9_-]{35}", commit_content))
-            
+            tokens = re.findall(r"\d{10}:[A-Za-z0-9_-]{35,}", commit_content)
+            print(tokens)
+            if tokens:
+                tg_tokens.update(tokens)
+                print(f"Found {len(tokens)} in ")
+            else:
+                print(f"No tokens found in {owner}/{repo}")
 
 
-    print(tg_tokens)
+    return tg_tokens
 
 
-get_recently_updated("bot")
+main("bot")
